a9d00863e4b2bcdf82f651f66257a119c69c9a3d
BonBon228 <nurdinnurtai@gmail.com>
2023-06-23 16:17:10 +0600

    DragAndDropBeautyAdded

--- Assets/Scripts/FlaskController.cs
+++ Assets/Scripts/FlaskController.cs
@@ -22,12 +22,13 @@ public class FlaskController : MonoBehaviour, IDragable
     {
         transform.SetParent(_hand.transform);
         transform.DOLocalMove(Vector3.zero, 0.25f);
+        transform.DOLocalRotate(Vector3.zero, 0.25f);
     }
 
     public void DropObject(Vector3 position)
     {
         transform.SetParent(null);
-        transform.DOMove(position, 0.3f);
-        transform.DORotate(Vector3.zero, 0.3f);
+        transform.DOMove(position, 0.25f);
+        transform.DORotate(new Vector3(0, transform.eulerAngles.y, 0), 0.25f);
     }
 }
